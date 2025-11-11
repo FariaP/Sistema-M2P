339bf28a5012543112b70e02c9fdc90dfe5ea2b3
Cotonho <marcoantoniomilagre587@gmail.com>
2025-11-11 14:45:42 -0300

    ajustes no sql

--- login/sql/mecanica.sql
+++ login/sql/mecanica.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 25, 2025 at 02:42 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Tempo de geração: 11/11/2025 às 18:45
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `mecanica`
+-- Banco de dados: `mecanica`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `item_servico`
+-- Estrutura para tabela `item_servico`
 --
 
 CREATE TABLE `item_servico` (
@@ -37,7 +37,7 @@ CREATE TABLE `item_servico` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pedido_servico`
+-- Estrutura para tabela `pedido_servico`
 --
 
 CREATE TABLE `pedido_servico` (
@@ -48,17 +48,10 @@ CREATE TABLE `pedido_servico` (
   `status` varchar(20) NOT NULL DEFAULT 'Em andamento'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `pedido_servico`
---
-
-INSERT INTO `pedido_servico` (`id`, `id_veiculo`, `data_criacao`, `observacoes`, `status`) VALUES
-(3, 3, '2025-10-25 12:33:52', 'Troca de óleo e filtro', 'Em andamento');
-
 -- --------------------------------------------------------
 
 --
--- Table structure for table `usuarios`
+-- Estrutura para tabela `usuarios`
 --
 
 CREATE TABLE `usuarios` (
@@ -69,21 +62,22 @@ CREATE TABLE `usuarios` (
   `placa_hash` varchar(255) NOT NULL,
   `criado_em` timestamp NOT NULL DEFAULT current_timestamp(),
   `tipo` enum('admin','user') NOT NULL DEFAULT 'user',
-  `placa` varchar(10) NOT NULL
+  `placa` varchar(10) NOT NULL,
+  `senha_hash` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `usuarios`
+-- Despejando dados para a tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nome`, `telefone`, `cpf_usuario`, `placa_hash`, `criado_em`, `tipo`, `placa`) VALUES
-(1, 'cotonho', '(43) 56436-5436', '123.456.789-00', '$2y$10$HBr5X1ThC8f9/ODwdRuNeugn6lVY9XRLl8ySMlFzeiixhiFISjACW', '2025-10-01 22:36:54', 'admin', 'ABC-1234'),
-(2, 'pedro', '(64) 13265-2457', '987.654.321-00', '$2y$10$dJhyVFFEXIB/C0WvuMvype0gokg0Op6Hy5JbnL1BNC3v/MP91gjH6', '2025-10-01 23:13:39', 'user', 'ABC-5678');
+INSERT INTO `usuarios` (`id`, `nome`, `telefone`, `cpf_usuario`, `placa_hash`, `criado_em`, `tipo`, `placa`, `senha_hash`) VALUES
+(3, 'teste teste', '(11) 11111-1111', '111.111.111-11', '$2y$10$XmfCu5/.MC/JaSutDJbutO1azAemLfV6zUyD5CT5KSllSrQ0JQ64m', '2025-11-11 16:58:15', 'admin', 'PCD-1234', '$2y$10$mfSgz7CfVERLO1WeqTldl.AUWN8VCTtdXLOoI1hqtKcQ2pn5NETTC'),
+(4, 'Pedro', '(22) 22222-2222', '222.222.222-22', '$2y$10$iO9QvYw00xS0nU1feZC6LOTFaiFkMHj4RTFSw25sHR1IyaU7bAKKy', '2025-11-11 17:26:23', 'user', 'ABC-1234', '$2y$10$DAsVFR0OuOulg.z4ZH77aedQ9pZSEttO2cSAsIvFXj6VZ8v2CWUEG');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `veiculo`
+-- Estrutura para tabela `veiculo`
 --
 
 CREATE TABLE `veiculo` (
@@ -95,90 +89,83 @@ CREATE TABLE `veiculo` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `veiculo`
---
-
-INSERT INTO `veiculo` (`id`, `cpf_usuario`, `placa`, `modelo`, `ano`) VALUES
-(3, '123.456.789-00', 'ABC-1234', 'Teste', 2020);
-
---
--- Indexes for dumped tables
+-- Índices para tabelas despejadas
 --
 
 --
--- Indexes for table `item_servico`
+-- Índices de tabela `item_servico`
 --
 ALTER TABLE `item_servico`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_pedido` (`id_pedido`);
 
 --
--- Indexes for table `pedido_servico`
+-- Índices de tabela `pedido_servico`
 --
 ALTER TABLE `pedido_servico`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_veiculo` (`id_veiculo`);
 
 --
--- Indexes for table `usuarios`
+-- Índices de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `cpf_usuario` (`cpf_usuario`);
 
 --
--- Indexes for table `veiculo`
+-- Índices de tabela `veiculo`
 --
 ALTER TABLE `veiculo`
   ADD PRIMARY KEY (`id`),
   ADD KEY `cpf_usuario` (`cpf_usuario`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT para tabelas despejadas
 --
 
 --
--- AUTO_INCREMENT for table `item_servico`
+-- AUTO_INCREMENT de tabela `item_servico`
 --
 ALTER TABLE `item_servico`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `pedido_servico`
+-- AUTO_INCREMENT de tabela `pedido_servico`
 --
 ALTER TABLE `pedido_servico`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `usuarios`
+-- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `veiculo`
---
-ALTER TABLE `veiculo`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- Constraints for dumped tables
+-- AUTO_INCREMENT de tabela `veiculo`
+--
+ALTER TABLE `veiculo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Restrições para tabelas despejadas
 --
 
 --
--- Constraints for table `item_servico`
+-- Restrições para tabelas `item_servico`
 --
 ALTER TABLE `item_servico`
   ADD CONSTRAINT `item_servico_ibfk_1` FOREIGN KEY (`id_pedido`) REFERENCES `pedido_servico` (`id`);
 
 --
--- Constraints for table `pedido_servico`
+-- Restrições para tabelas `pedido_servico`
 --
 ALTER TABLE `pedido_servico`
   ADD CONSTRAINT `pedido_servico_ibfk_1` FOREIGN KEY (`id_veiculo`) REFERENCES `veiculo` (`id`);
 
 --
--- Constraints for table `veiculo`
+-- Restrições para tabelas `veiculo`
 --
 ALTER TABLE `veiculo`
   ADD CONSTRAINT `veiculo_ibfk_1` FOREIGN KEY (`cpf_usuario`) REFERENCES `usuarios` (`cpf_usuario`);
